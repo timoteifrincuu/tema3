90b0854327bc537ea7c90c4a7974fde8f7814a0c
unknown <timotei.frincu@emanuel.ro>
2023-10-11 21:25:53 +0300

    bitwise

---
+++ tema3.cs
@@ -0,0 +1,21 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("------------------");
+        Console.WriteLine("| A | B | A & B |");
+        Console.WriteLine("------------------");
+
+        for(int A = 0;A as <=1; A++)
+        {
+            for (int B = 0; B <= 1; B++)
+            {
+                int rezultat = A & B;
+                Console.WriteLine($"| {A} | {B} | {rezultat} |");
+            }
+            Console.WriteLine("------------------");
+        }
+    }
+}
